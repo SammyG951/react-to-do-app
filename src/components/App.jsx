ccfb4a435968ea2de4cf943cd9b01c12f7b29aa0
Sammy Gonzalez III <sammygonzo951@gmail.com>
2023-10-18 19:39:18 -0700

    Redid starting files

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./partials/Header.jsx";
+import Footer from "./partials/Footer.jsx";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <p>Hello World</p>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
